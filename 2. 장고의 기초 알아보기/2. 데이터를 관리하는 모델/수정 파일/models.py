f46e6775f007e693f96be26974c99ec67c883ab9
DongHee Kim <ehdgml7755@cu.ac.kr>
2023-02-28 23:16:23 +0900

    commit

---
+++ 2. 장고의 기초 알아보기/2. 데이터를 관리하는 모델/수정 파일/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+# Question Model
+class Question(models.Model):
+    # 질문의 제목
+    subject = models.CharField(max_length=200)
+    # 질문의 내용
+    content = models.TextField()
+    # 작성일시
+    create_date = models.DateTimeField()
+
+# Answer Model
+class Answer(models.Model):
+    # 질문
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # 답변의 내용
+    content = models.TextField()
+    # 답변을 작성한 일시
+    create_date = models.DateTimeField()
